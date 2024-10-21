cb115a9fdc59cb470bb5ee3d8e28ee2d97bc1fc8
n3zushi <9836@holbertonstudents.com>
2024-10-21 14:26:52 +0200

    update 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,6 @@
 #include <unistd.h>
 
 /**
- * ft_putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-void ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
