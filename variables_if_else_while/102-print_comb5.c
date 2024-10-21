43cfa9141c4476fbcf5007b1c274f5e2fe47af5f
n3zushi <9836@holbertonstudents.com>
2024-10-21 16:03:22 +0200

    ajout du 3 eme extra du projet 2 semaines 3

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c, C, D, E;
+
+	for (c = 0; c <= 99; c++)
+	{
+		for (C = c + 1; C <= 99; C++)
+		{
+			putchar((c / 10) + '0');
+			putchar((c % 10) + '0');
+			putchar(' ');
+			putchar((C / 10) + '0');
+			putchar((C % 10) + '0');
+
+			if (c != 98 || C != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
