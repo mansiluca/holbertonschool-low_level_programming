efa537c409d498392b2cc361b6e50a2b63d156f4
n3zushi <9836@holbertonstudents.com>
2024-10-21 15:12:14 +0200

    correction du 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -11,20 +11,15 @@ int main(void)
 	char c = '0';
 	char C = 'a';
 
-	if (c <= '9')
+	while (c <= '9')
 	{
-		while (c <= '9')
-		{
-			putchar(c);
-			c++;
-		}
-	} else
+		putchar(c);
+		c++;
+	}
+	while (C <= 'f')
 	{
-		while (C <= 'f')
-		{
-			putchar(C);
-			C++;
-		}
+		putchar(C);
+		C++;
 	}
 	putchar('\n');
 	return (0);
