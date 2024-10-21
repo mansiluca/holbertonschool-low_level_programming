445a7ff7407c24b94a6551357b4d4e07870b6b3d
n3zushi <9836@holbertonstudents.com>
2024-10-21 14:29:20 +0200

    update 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -12,9 +12,9 @@ int main(void)
 
 	while (c <= 'z')
 	{
-		ft_putchar(c);
+		putchar(c);
 		c++;
 	}
-	ft_putchar('\n');
+	putchar('\n');
 	return (0);
 }
