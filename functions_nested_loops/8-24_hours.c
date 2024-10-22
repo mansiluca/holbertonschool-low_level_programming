52fd8032c8f93c98237c30ab62111f7475d75cdc
n3zushi <9836@holbertonstudents.com>
2024-10-22 10:30:42 +0200

    modification du header et gitignore et ajout du 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			putchar((h / 10) + '0');
+			putchar((h % 10) + '0');
+			putchar(':');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+
+			if (h != 23 || m != 59)
+			{
+				putchar('\n');
+			}
+		}
+	}
+	putchar('\n');
+}
