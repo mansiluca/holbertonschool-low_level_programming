b63ddfc9704482f4752d38a28d31bb8f33510189
n3zushi <9836@holbertonstudents.com>
2024-10-22 11:10:57 +0200

    modification du header et git ignore et ajout du 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - that prints the 9 times table, starting with 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int i, k;
+	int count = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			count = i * k;
+			if (count < 10)
+			{
+				_putchar(count + '0');
+			}
+			else
+			{
+				_putchar((count / 10) + '0');
+				_putchar((count % 10) + '0');
+			}
+			if (k != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
