b8a3c8dd0aa0038022e3e001e37fa40d979177cb
n3zushi <9836@holbertonstudents.com>
2024-10-22 12:54:38 +0200

    update header et ajout du termier exo du 3eme projet

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98.
+ *
+ * @n: int
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	for (n = n; n < 99; n++)
+	{
+		printf("%d", n);
+		if (n != 98)
+		{
+			printf(", ");
+		}
+	}
+}
