e5e38b4eb09330c582e79de4ab02b81e6c8bbcba
n3zushi <9836@holbertonstudents.com>
2024-10-22 12:47:31 +0200

    update header et exo 10 ajouter

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
