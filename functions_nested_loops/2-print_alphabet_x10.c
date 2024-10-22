b615b7dbfef5c4503314744bd8d90aa934726b88
n3zushi <9836@holbertonstudents.com>
2024-10-22 09:33:02 +0200

    correction du 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,20 @@
 
 /**
  * print_alphabet_x10 - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void);
-
 void print_alphabet_x10(void)
 {
-	int i;
-	for (i = 0; i < 10; i++)
-		print_alphabet();
+	int c, C;
+
+	for (C = 0; C < 10; C++)
+	{
+		for (c = 97; c <= 122; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
