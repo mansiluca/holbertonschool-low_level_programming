20ec3fbb5ceb347663bf78127441544a77b86759
n3zushi <9836@holbertonstudents.com>
2024-10-28 16:59:06 +0100

    ajout de l'exo 7 et update du main.h

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "unistd.h"
+#include "stdio.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int len = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	for (i = len / 2; i < len; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
