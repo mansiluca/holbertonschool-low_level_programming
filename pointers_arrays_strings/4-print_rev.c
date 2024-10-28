1f0b9618978cc44578e6e8d15cc16bf4f6bf15c5
n3zushi <9836@holbertonstudents.com>
2024-10-28 15:01:33 +0100

    ajout du 4 et update du main.h

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		putchar(s[i]);
+		i--;
+	}
+}
