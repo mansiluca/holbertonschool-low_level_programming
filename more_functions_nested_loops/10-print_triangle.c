a2c81dc42c5e6642302bde2c120151621679dbc3
n3zushi <9836@holbertonstudents.com>
2024-10-28 12:05:51 +0100

    ajout du 10 et update du main.h

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: The size of the triangle
+ *
+ * Return: On success 1.
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+	int count = size;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+	for (i = 0; i < count; i++)
+	{
+		for (j = 0; j < count - i - 1; j++)
+		{
+			putchar(' ');
+		}
+		for (k = 0; k < i + 1; k++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+}
