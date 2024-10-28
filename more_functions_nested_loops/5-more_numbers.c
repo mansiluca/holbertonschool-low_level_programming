4d7ca1b6a140e627e8e57bca83aad6b454f3b4ba
n3zushi <9836@holbertonstudents.com>
2024-10-28 10:09:15 +0100

    update 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,19 +10,16 @@ void more_numbers(void)
 {
 	int i, j = 0;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		while (j < 15)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 			{
 				_putchar((j / 10) + '0');
 			}
 			_putchar((j % 10) + '0');
-			j++;
 		}
 		_putchar('\n');
-		j = 0;
-		i++;
 	}
 }
