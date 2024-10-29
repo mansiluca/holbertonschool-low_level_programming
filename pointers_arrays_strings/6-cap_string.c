77ca129185841a1f6e99af219dc2063f0f8b4637
n3zushi <9836@holbertonstudents.com>
2024-10-29 09:55:30 +0100

    ajout de cap-string et update du main.h

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to capitalize
+ *
+ * Return: address of str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char sep[] = " \t\n,;.!?\"(){}";
+	int count = 0;
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+	while (str[count])
+	{
+		count++;
+	}
+	for (i = 0; i < count; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			for (int j = 0; sep[j]; j++)
+			{
+				if (str[i - 1] == sep[j])
+				{
+					str[i] -= 32;
+					break;
+				}
+			}
+		}
+	}
+	return (str);
+}
