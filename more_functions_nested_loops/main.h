fe15047c84105c4ac76f52ccbde0a6a9cee206ab
n3zushi <9836@holbertonstudents.com>
2024-10-29 10:47:00 +0100

    update

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-
 void _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
