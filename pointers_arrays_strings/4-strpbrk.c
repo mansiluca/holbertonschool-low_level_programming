ed41460ec2e608ac361c633f027a12c437404415
n3zushi <9836@holbertonstudents.com>
2024-11-04 13:08:04 +0100

    update du memset strchr strpbrk print chessboard

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -24,21 +24,3 @@ char *_strpbrk(char *s, char *accept)
 	}
 	return (0);
 }
-
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *s = "hello, world";
-	char *f = " ";
-	char *t;
-
-	t = _strpbrk(s, f);
-	printf("%s\n", t);
-	return (0);
-}
--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -7,18 +7,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j = 0;
+	int i, j;
 
-	while (a[i][j] != '\0')
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(a[i][j]);
-		j++;
-		if (j == 8)
+		for (j = 0; j < 8; j++)
 		{
-			_putchar('\n');
-			j = 0;
-			i++;
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
