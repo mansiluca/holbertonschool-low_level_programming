1617b2cf1eb268fd307867f09dba9e854c253402
n3zushi <9836@holbertonstudents.com>
2024-11-06 09:45:31 +0100

    ajout de 2-args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * All arguments should be printed, including the first one
+ * one argument per line, ending with a new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+}
