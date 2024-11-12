59f1e2dbb2a36b99abf71bf5a5f74e0fcce6cb86
n3zushi <9836@holbertonstudents.com>
2024-11-12 17:16:54 +0100

    correctif sur un if

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -20,6 +20,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
 	{
 		ptr[i] = 0;
