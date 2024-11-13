e8850162a6287f6bee9f680f97835b34943962ee
n3zushi <9836@holbertonstudents.com>
2024-11-13 10:28:05 +0100

    ajout de vérifications de nullité et correction de fautes dans les fonctions de gestion des chiens

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	pritnf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+	printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner; %s\n", d->owner == NULL ? "(nil)" : d->owner);
+	printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 }
--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,6 +4,45 @@
 #include <string.h>
 
 /**
+ * _strcpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+
+int _strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (i);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -16,26 +55,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(strlen(name) + 1);
+	dog->name = malloc(_strlen(name) + 1);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	strcpy(dog->name, name);
+	_strcpy(dog->name, name);
 
-	dog->owner = malloc(strlen(owner) + 1);
+	dog->owner = malloc(_strlen(owner) + 1);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	strcpy(dog->owner, owner);
+	_strcpy(dog->owner, owner);
 
 	dog->age = age;
 
