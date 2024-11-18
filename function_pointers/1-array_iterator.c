a63a7f5b6fb3db78007f646ca5de69a416d842a2
NezuShi <luca.mozart06@gmail.com>
2024-11-18 13:48:53 +0100

    refactor: reorder includes and improve null checks in array_iterator function

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
-#include "function_pointers.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * array_iterator - executes a function given as a parameter on each element of an array
@@ -15,10 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	if (array == NULL || action == NULL)
-	{
-		return;
-	}
 	while (i < size)
 	{
 		action(array[i]);
