91b63f1ce07b3c1b25d4d184f26a4a67484b7c90
n3zushi <9836@holbertonstudents.com>
2024-12-05 14:00:55 +0100

    feat: ajouter une fonction _strlen pour calculer la longueur des chaînes dans add_node et add_node_end

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to the head of the list
  * @str: string to be added to the list
@@ -23,7 +41,7 @@ list_t *add_node(list_t **head, const char *str)
 		i++;
 	}
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer to the head of the list
  * @str: string to be added to the list
@@ -24,7 +42,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		i++;
 	}
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = NULL;
 	if (*head == NULL)
 	{
