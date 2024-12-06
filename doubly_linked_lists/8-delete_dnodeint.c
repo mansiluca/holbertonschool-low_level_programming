c84c03aa16f5095d5150641d0c8660628395eeb4
NezuShi <luca.mozart06@gmail.com>
2024-12-06 12:21:08 +0100

    style: format code in delete_dnodeint_at_index function for improved readability

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,36 +11,36 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    unsigned int i = 0;
+	dlistint_t *current = *head;
+	unsigned int i = 0;
 
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+		return (-1);
 
-    if (index == 0)
-    {
-        *head = current->next;
-        if (current->next != NULL)
-            current->next->prev = NULL;
-        free(current);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = current->next;
+		if (current->next != NULL)
+			current->next->prev = NULL;
+		free(current);
+		return (1);
+	}
 
-    while (current != NULL && i < index)
-    {
-        current = current->next;
-        i++;
-    }
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
 
-    if (current == NULL)
-        return (-1);
+	if (current == NULL)
+		return (-1);
 
-    if (current->next != NULL)
-        current->next->prev = current->prev;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
 
-    if (current->prev != NULL)
-        current->prev->next = current->next;
+	if (current->prev != NULL)
+		current->prev->next = current->next;
 
-    free(current);
-    return (1);
+	free(current);
+	return (1);
 }
