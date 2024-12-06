24306701b9339722a984a39e487c0302cac7a87b
NezuShi <nezushi@nezushi.fr>
2024-12-06 19:32:29 +0100

    feat: rename append_text_to_file to create_file and update documentation

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,13 +3,13 @@
 #include <unistd.h>
 
 /**
- * append_text_to_file - appends text at the end of a file
+ * create_file - creates a file
  * @filename: the name of the file
- * @text_content: the NULL terminated string to add at the end of the file
+ * @text_content: the NULL terminated string to write to the file
  * Return: 1 on success and -1 on failure
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0, written;
 
