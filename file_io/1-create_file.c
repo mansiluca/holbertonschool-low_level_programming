40a0590896282f2c39b1fc6b15ca79e1b5a4e4de
NezuShi <nezushi@nezushi.fr>
2024-12-06 19:34:25 +0100

    feat: update create_file function to use O_CREAT and O_TRUNC for file creation

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,18 +16,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
-	}
-
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
+		while (text_content[len])
+			len++;
+
 		written = write(fd, text_content, len);
 		if (written == -1)
 		{
