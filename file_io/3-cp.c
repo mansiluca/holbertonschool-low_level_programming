ca8251256d1d7f7faaf6cb3ff5ce684cbf6385a0
NezuShi <nezushi@nezushi.fr>
2024-12-09 09:49:09 +0100

    feat: refactor copy_file function by extracting content copying logic to copy_content function for improved readability and maintainability

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,6 +18,9 @@ void handle_error(const char *message, const char *value, int exit_code)
 	exit(exit_code);
 }
 
+void copy_content(int fd_from, int fd_to,
+const char *file_from, const char *file_to);
+
 /**
  * copy_file - Copies the content of a file to another file
  * @file_from: The file to copy from
@@ -25,28 +28,47 @@ void handle_error(const char *message, const char *value, int exit_code)
  */
 void copy_file(const char *file_from, const char *file_to)
 {
-	int fd_from, fd_to, read_bytes, write_bytes;
+	int fd_from, fd_to;
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		handle_error("Error: Can't read from file %s\n", file_from, 98);
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		handle_error("Error: Can't write to %s\n", file_to, 99);
+	}
+
+
+	copy_content(fd_from, fd_to, file_from, file_to);
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+}
+
+/**
+ * copy_content - Reads from one file descriptor and writes to another
+ * @fd_from: The file descriptor to read from
+ * @fd_to: The file descriptor to write to
+ * @file_from: The file to copy from
+ * @file_to: The file to copy to
+ */
+void copy_content(int fd_from, int fd_to,
+const char *file_from, const char *file_to)
+{
+	int read_bytes, write_bytes;
 	char buffer[BUFFER_SIZE];
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-		handle_error("Error: Can't read from file %s\n", file_from, 98);
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close(fd_from);
-		handle_error("Error: Can't write to %s\n", file_to, 99);
-	}
-
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-		handle_error("Error: Can't read from file %s\n", file_from, 98);
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close(fd_from);
-		handle_error("Error: Can't write to %s\n", file_to, 99);
-	}
 	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		write_bytes = write(fd_to, buffer, read_bytes);
@@ -59,16 +81,6 @@ void copy_file(const char *file_from, const char *file_to)
 	}
 	if (read_bytes == -1)
 		handle_error("Error: Can't read from file %s\n", file_from, 98);
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
 }
 
 /**
