cdf30084ad49c5e29cf6a91f399a32798e4052d9
NezuShi <nezushi@nezushi.fr>
2024-12-09 09:25:41 +0100

    feat: streamline error handling in copy_file function by removing redundant checks

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,14 +37,12 @@ void copy_file(const char *file_from, const char *file_to)
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 		handle_error("Error: Can't read from file %s\n", file_from, 98);
-
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
 		handle_error("Error: Can't write to %s\n", file_to, 99);
 	}
-
 	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		write_bytes = write(fd_to, buffer, read_bytes);
@@ -55,16 +53,13 @@ void copy_file(const char *file_from, const char *file_to)
 			handle_error("Error: Can't write to %s\n", file_to, 99);
 		}
 	}
-
 	if (read_bytes == -1)
 		handle_error("Error: Can't read from file %s\n", file_from, 98);
-
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
